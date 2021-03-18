d65fd3f27f88a192816f4c81089ad11412765902
Sid <1@2.ru>
2021-03-18 14:16:32 +0400

    Add menu and toggle-menu btn --fix

--- dest/scripts/main.js
+++ dest/scripts/main.js
@@ -3,19 +3,45 @@
 const toggleButton = document.querySelector('.button-toggle')
 const navigationElement = document.querySelector('.navigation')
 
-const removeNoJsClass = () => {
-    navigationElement.classList.remove('navigation--no-js')
-}
 
-const toggleMenu = () => {
-    if (navigationElement.classList.contains('navigation--opened')) {
-        navigationElement.classList.remove('navigation--opened')
-        navigationElement.classList.add('navigation--closed')
-    } else {
-        navigationElement.classList.remove('navigation--closed')
-        navigationElement.classList.add('navigation--opened')
+class MobileMenuClass {
+    constructor(menuButton, navigationMenu, classes = {}) {
+        this.menuButton = menuButton;
+        this.navigationMenu = navigationMenu; 
+        this.openClass = classes.open;
+        this.closeClass = classes.close;
+        this.noJsClass = classes.noJs 
+    }
+
+    initialize = () => {
+        this.removeNoJsClass();
+        this.menuButton.addEventListener('click', this.toggleMenu)
+    }
+
+    removeNoJsClass = () => {
+        this.navigationMenu.classList.remove(this.noJsClass)
+    }
+
+    changeClass = (remove, add) => {
+        this.navigationMenu.classList.remove(remove)
+        this.navigationMenu.classList.add(add)
+        console.log('switched')
+    }
+
+    toggleMenu = () => {
+        console.log(this.openClass)
+        if (this.navigationMenu.classList.contains(this.openClass)) {
+            this.changeClass(this.openClass, this.closeClass)
+        } else {
+            this.changeClass(this.closeClass, this.openClass)
+        }
     }
 }
 
-removeNoJsClass()
-toggleButton.addEventListener('click', toggleMenu)
+const mobileMenu = new MobileMenuClass(toggleButton, navigationElement, {
+    open: 'navigation--opened',
+    close: 'navigation--closed',
+    noJs: 'navigation--no-js'
+})
+
+mobileMenu.initialize()
